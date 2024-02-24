3798479392058d77058dc0335ee1d1bb4158c538
jsmelchorg <160347054+jsmelchorg@users.noreply.github.com>
2024-02-24 19:28:48 +0000

    cambio de html

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Ya estoy editando mi primer aplicacion we.
         </p>
         <a
           className="App-link"
